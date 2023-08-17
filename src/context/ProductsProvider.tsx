b4654bebe3cefcc9d5170a2b371e666ee93cf240
Ilya <ilya.shuvatov1@gmail.com>
2023-08-17 17:56:23 +0200

    completed cart

---
+++ src/context/ProductsProvider.tsx
@@ -0,0 +1,60 @@
+import {createContext, ReactElement, useState, useEffect} from "react";
+
+export type ProductType = {
+    sku: string,
+    name: string,
+    price: number,
+}
+
+// const initState: ProductType[] = []
+
+const initState: ProductType[] = [
+    {
+        "sku": "item0001",
+        "name": "New Balance 1906R",
+        "price": 139.5
+    },
+    {
+        "sku": "item0002",
+        "name": "New Balance 2002R",
+        "price": 150.99
+    },
+    {
+        "sku": "item0003",
+        "name": "New Balance 9006",
+        "price": 119.75
+    }
+]
+
+export type UseProductsContextType = { products: ProductType[] }
+
+const initContextState: UseProductsContextType = { products: [] }
+
+const ProductsContext = createContext<UseProductsContextType>(initContextState)
+
+type ChildrenType = { children?: ReactElement | ReactElement[] }
+
+export const ProductsProvider = ({ children }: ChildrenType): ReactElement => {
+    const [products, setProducts] = useState<ProductType[]>(initState)
+
+    // useEffect(() => {
+    //    const fetchProducts = async() => {
+    //        const data = await fetch("http://localhost:3500/products").then(res => {
+    //            return res.json()
+    //        }).catch(err => {
+    //            if (err instanceof Error) console.log(err.message)
+    //        })
+    //         return data
+    //     }
+    //
+    //     fetchProducts().then(products => setProducts(products))
+    // }, [])
+
+    return (
+        <ProductsContext.Provider value={{ products }}>
+            { children }
+        </ProductsContext.Provider>
+    )
+}
+
+export default ProductsContext
